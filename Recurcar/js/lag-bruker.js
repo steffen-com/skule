b6e8bae226f79da43948fd24c43a25db10d65bca
steffen-com <e-steaga@iskule.no>
2023-05-10 12:37:31 +0200

    s

--- Recurcar/js/lag-bruker.js
+++ Recurcar/js/lag-bruker.js
@@ -69,3 +69,178 @@ function login() {
             });
     }
 
+    function signUp1() {
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const fname = document.getElementById("fname").value;
+        const lname = document.getElementById("lname").value;
+
+        const zip = document.getElementById("zip").value;
+        const city = document.getElementById("city").value;
+    
+        // Oppretter bruker som kan logge seg på firebase og få tilgang til nettstaden
+        auth.createUserWithEmailAndPassword(email, password)
+            // Lagrer også brukeren i collection "users"
+            .then((userCredentials) => {
+                sessionStorage.setItem("uid", userCredentials.user.uid)
+                db.collection("Admin").doc().set({
+                    namn: fname,
+                    brukernamn: lname,
+
+                    tlf: zip,
+                    alderPc: city,
+                    systemModel: email,
+                    userId: userCredentials.user.uid
+                })
+                    .then(function () {
+                        window.location.href = "./Admin.html";
+                    })
+            })
+    
+            .catch((err) => {
+                alert(err.message)
+                console.log(err.code);
+                console.log(err.message);
+            });
+    }
+
+    function signUp2() {
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const fname = document.getElementById("fname").value;
+        const lname = document.getElementById("lname").value;
+
+        const zip = document.getElementById("zip").value;
+        const city = document.getElementById("city").value;
+    
+        // Oppretter bruker som kan logge seg på firebase og få tilgang til nettstaden
+        auth.createUserWithEmailAndPassword(email, password)
+            // Lagrer også brukeren i collection "users"
+            .then((userCredentials) => {
+                sessionStorage.setItem("uid", userCredentials.user.uid)
+                db.collection("Lerara").doc().set({
+                    namn: fname,
+                    brukernamn: lname,
+
+                    tlf: zip,
+                    alderPc: city,
+                    systemModel: email,
+                    userId: userCredentials.user.uid
+                })
+                    .then(function () {
+                        window.location.href = "./Lerar.html";
+                    })
+            })
+    
+            .catch((err) => {
+                alert(err.message)
+                console.log(err.code);
+                console.log(err.message);
+            });
+    }
+
+    function signUp3() {
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const fname = document.getElementById("fname").value;
+        const lname = document.getElementById("lname").value;
+
+        const zip = document.getElementById("zip").value;
+        const city = document.getElementById("city").value;
+    
+        // Oppretter bruker som kan logge seg på firebase og få tilgang til nettstaden
+        auth.createUserWithEmailAndPassword(email, password)
+            // Lagrer også brukeren i collection "users"
+            .then((userCredentials) => {
+                sessionStorage.setItem("uid", userCredentials.user.uid)
+                db.collection("Elev").doc().set({
+                    namn: fname,
+                    brukernamn: lname,
+
+                    tlf: zip,
+                    alderPc: city,
+                    systemModel: email,
+                    userId: userCredentials.user.uid
+                })
+                    .then(function () {
+                        window.location.href = "./Elev.html";
+                    })
+            })
+    
+            .catch((err) => {
+                alert(err.message)
+                console.log(err.code);
+                console.log(err.message);
+            });
+    }
+
+    function signUp4() {
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const fname = document.getElementById("fname").value;
+        const lname = document.getElementById("lname").value;
+
+        const zip = document.getElementById("zip").value;
+        const city = document.getElementById("city").value;
+    
+        // Oppretter bruker som kan logge seg på firebase og få tilgang til nettstaden
+        auth.createUserWithEmailAndPassword(email, password)
+            // Lagrer også brukeren i collection "users"
+            .then((userCredentials) => {
+                sessionStorage.setItem("uid", userCredentials.user.uid)
+                db.collection("Milijofagarbeidar").doc().set({
+                    namn: fname,
+                    brukernamn: lname,
+
+                    tlf: zip,
+                    alderPc: city,
+                    systemModel: email,
+                    userId: userCredentials.user.uid
+                })
+                    .then(function () {
+                        window.location.href = "./Miljøfagarbeidar.html";
+                    })
+            })
+    
+            .catch((err) => {
+                alert(err.message)
+                console.log(err.code);
+                console.log(err.message);
+            });
+    }
+
+    function signUp5() {
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const fname = document.getElementById("fname").value;
+        const lname = document.getElementById("lname").value;
+
+        const zip = document.getElementById("zip").value;
+        const city = document.getElementById("city").value;
+    
+        // Oppretter bruker som kan logge seg på firebase og få tilgang til nettstaden
+        auth.createUserWithEmailAndPassword(email, password)
+            // Lagrer også brukeren i collection "users"
+            .then((userCredentials) => {
+                sessionStorage.setItem("uid", userCredentials.user.uid)
+                db.collection("IT-medarbeidar").doc().set({
+                    namn: fname,
+                    brukernamn: lname,
+
+                    tlf: zip,
+                    alderPc: city,
+                    systemModel: email,
+                    userId: userCredentials.user.uid
+                })
+                    .then(function () {
+                        window.location.href = "./IT-medarbeidar.html";
+                    })
+            })
+    
+            .catch((err) => {
+                alert(err.message)
+                console.log(err.code);
+                console.log(err.message);
+            });
+    }
+
